ef5e1b5f7db8d4fcc827931683e357b71ed5c0f9
SIRSTN <stephane.velen@gmail.com>
2024-05-17 15:39:40 +0200

    cloud commit

--- app.py
+++ app.py
@@ -247,4 +247,4 @@ def store_text():
         return jsonify({'ids': ids, 'msg': 'Texts stored, no average sentiment stored due to null or zero values'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5003)
